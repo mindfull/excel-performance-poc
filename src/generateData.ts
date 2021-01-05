29f0c733bcc5fb4194d5f3a83968fca2d39622f7
Tae-geon Choi <contact@zfbe.com>
2021-01-05 19:56:43 +0900

    feat: 데이터 날짜 범위 랜덤화

--- src/generateData.ts
+++ src/generateData.ts
@@ -13,20 +13,32 @@ export interface Data {
   paymentType: string;
 }
 
-const generateData = (length: number = 10000): Data[] =>
-  Array.apply(undefined, Array(length)).map(() => ({
-    orderNumber: `20200101010101001#${Math.floor(Math.random() * 10000)}`,
-    date: "2020-01-01",
-    clientCharge:
-      Math.random() > 0.8 ? 15000 + Math.floor(Math.random() * 10) * 1000 : 0,
-    cancelFee: 0,
-    deliveryFee: 3500 + Math.floor(Math.random() * 100) * 10,
-    creditCardCharge:
-      Math.random() > 0.5 ? 15000 + Math.floor(Math.random() * 10) * 1000 : 0,
-    creditCardFee: 0,
-    creditCardFeeRate: 0,
-    cashReceiptId: undefined,
-    paymentType: paymentTypes[Math.floor((Math.random() * 3) % 3)],
-  }));
+const generateData = (length: number = 10000): Data[] => {
+  const res = Array.apply(undefined, Array(length)).map(() => {
+    const month = (Math.floor(Math.random() * 12) + 1)
+      .toString()
+      .padStart(2, "0");
+    const date = (Math.floor(Math.random() * 28) + 1)
+      .toString()
+      .padStart(2, "0");
+    return {
+      orderNumber: `2020${month}${date}010101001#${Math.floor(
+        Math.random() * 10000
+      )}`,
+      date: `2020-${month}-${date}`,
+      clientCharge:
+        Math.random() > 0.8 ? 15000 + Math.floor(Math.random() * 10) * 1000 : 0,
+      cancelFee: 0,
+      deliveryFee: 3500 + Math.floor(Math.random() * 100) * 10,
+      creditCardCharge:
+        Math.random() > 0.5 ? 15000 + Math.floor(Math.random() * 10) * 1000 : 0,
+      creditCardFee: 0,
+      creditCardFeeRate: 0,
+      cashReceiptId: undefined,
+      paymentType: paymentTypes[Math.floor((Math.random() * 3) % 3)],
+    };
+  });
+  return res.sort((a, b) => a.date.localeCompare(b.date));
+};
 
 export default generateData;
